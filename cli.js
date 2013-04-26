b99acac8c9add733342e2965814bb16df5f48e19
Helder Santana <helder.css@gmail.com>
2013-04-26 00:14:00 -0300

    improve getters and setters

--- cli.js
+++ cli.js
@@ -1,56 +1,13 @@
 /*
- * datauri - A simple Data URI scheme generator
- * https://github.com/heldr/datauri
+ * mimetyper
+ * https://github.com/heldr/mimetyper
  *
- * Copyright (c) 2012 Helder Santana
+ * Copyright (c) 2013 Helder Santana
  * Licensed under the MIT license.
- * https://raw.github.com/heldr/datauri/master/MIT-LICENSE.txt
+ * https://raw.github.com/heldr/mimetyper/master/MIT-LICENSE.txt
  */
 
-var args    = process.argv,
-    fs      = require('fs'),
-    path    = require('path'),
-    DataURI = require('./datauri');
+var args = process.argv,
+    Mimetyper = require('./mimetyper');
 
-function writeNewCssFile( file , content , action ) {
-    fs.writeFile( file , content , 'utf-8' , function(err) {
-        if(err) {
-            throw err;
-        }
-
-        console.log('File ' + action + ': ' + file);
-    });
-}
-
-function outputCSS( file , content ) {
-
-    if ( fs.existsSync(file) ) {
-
-        if ( path.extname(file).match(/(css|sass|less)/) ) {
-            fs.readFile( file , 'utf-8' , function( err , cssContent ) {
-
-                cssContent += content;
-
-                writeNewCssFile( file , cssContent , 'updated' );
-
-            });
-        } else {
-            console.log( 'Must be a CSS file' );
-        }
-
-
-    } else {
-
-        writeNewCssFile( file , content , 'created' );
-
-    }
-}
-
-if (args.length > 2) {
-
-    var cls = args[4] || '',
-        uri = new DataURI( args[2] );
-
-    ( args.length < 4 ) ? console.log( uri.content ) : outputCSS( args[3] , uri.getCss( cls ) );
-
-}
+console.log( '"' + Mimetyper( process.argv[2] ) + '"' );
--- test/run.js
+++ test/run.js
@@ -1,10 +1,31 @@
 var Mimetyper = require("../lib/exec.js"),
+    instance = Mimetyper(),
     assert  = require("assert"),
     _       = require("lodash"),
     extList = Mimetyper().list,
     path = '/i/am/a/simple/path/file.',
     url = 'http://fake.com/umk/files/file.',
     cases = {}, expected;
+
+
+cases['should create a extension without dot'] = function () {
+    instance.set('fake','text/fake');
+
+    assert.strictEqual( instance.get('file.fake') , 'text/fake');
+}
+
+cases['should create a extension with dot'] = function () {
+    instance.set('.fake2','text/fake');
+
+    assert.strictEqual( instance.get('file.fake2') , 'text/fake');
+}
+
+cases['should create multiple extensions'] = function () {
+    instance.set(['.audi','tesla'],'vehicle/car');
+
+    assert.strictEqual( instance.get('file.audi') , 'vehicle/car');
+    assert.strictEqual( instance.get('file.tesla') , 'vehicle/car');
+}
 
 for (var ext in extList) {
     if (extList.hasOwnProperty(ext)) {
@@ -19,14 +40,15 @@ for (var ext in extList) {
         };
 
         cases['should run as object method and get extension .' + ext + ' from ' + path + ext] = function () {
-            assert.strictEqual( Mimetyper().get(path + ext) , expected);
+            assert.strictEqual( instance.get(path + ext) , expected);
         };
 
         cases['should run as object method and get extension .' + ext + ' from ' + url + ext] = function () {
-            assert.strictEqual( Mimetyper().get(url + ext) , expected);
+            assert.strictEqual( instance.get(url + ext) , expected);
         };
     }
 }
+
 
 var pairs = _.pairs(cases), last = _.last(pairs);
 
